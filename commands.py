93237d49607f45efd5491c8e8ec85512d1b025b6
Raidienaty <jcanducci@gmail.com>
2022-05-16 14:30:11 -0400

    Basic check for hey Jeeves method built

---
+++ commands.py
@@ -0,0 +1,14 @@
+import re
+
+class Commands:
+
+    def __init__(self) -> None:
+        self.called = False
+        pass
+
+    def checkCall(self, request):
+        if re.search(r'\b(Hey|hello|yo|hi) Jeeves', request, re.I):
+            self.called = True
+
+    def evaluateRequest():
+        pass
