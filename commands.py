1601c3f3f9f5520f8e47301405e79bdb859f43db
Raidienaty <jcanducci@gmail.com>
2022-05-16 14:49:04 -0400

    Added how are you system

--- commands.py
+++ commands.py
@@ -1,14 +1,22 @@
 import re
 
+from numpy import require
+from requests import request
+
 class Commands:
 
     def __init__(self) -> None:
         self.called = False
-        pass
 
     def checkCall(self, request):
         if re.search(r'\b(Hey|hello|yo|hi) Jeeves', request, re.I):
             self.called = True
 
     def evaluateRequest(self, request):
-        pass
+        if re.search(r'\b[\d]+(+|*|/|-)[\d]+'):
+            print('Attempt to do math')
+        elif re.search(r'\bHow are you', request, re.I):
+            print('I\'m doing well, how are you?')
+            self.hru = True
+        elif self.hru:
+            print('What can I do for you?')
